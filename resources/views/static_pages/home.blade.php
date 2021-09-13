b85eaf23cbeb555db80aba3afd333c3b748a7d1f
Hershel Zhang <wszhhx@163.com>
2021-09-13 15:19:18 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,7 +5,7 @@
     <h1>Hello Hershel</h1>
     <p class="lead">你现在所看到的是微博类网站示例主页</p>
     <p>全栈，将从这里开始。。。</p>
-    <p><a class="btn btn-lg btn-success" href="#" role="button">现在注册</a></p>
+    <p><a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a></p>
   </div>
 @stop
 
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('/signup', 'UserController@create')->name('signup');
