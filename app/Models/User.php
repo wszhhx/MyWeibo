fd6373e89cb68e9db373dcc12c660aa66c1cb40e
Hershel Zhang <wszhhx@163.com>
2021-09-14 16:43:07 +0000

    5-5.5 over

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,21 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;      //模型工厂相关功能引用
+use Illuminate\Foundation\Auth\User as Authenticatable;     //授权相关功能引用
+use Illuminate\Notifications\Notifiable;                    //消息通知相关功能引用
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
      * @var string[]
      */
-    protected $fillable = [
+    protected $fillable = [     //过滤掉用户提交的其他字段，只有包含在该属性中的字段才能够被正常更新
         'name',
         'email',
         'password',
@@ -28,7 +29,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $hidden = [
+    protected $hidden = [       //对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏
         'password',
         'remember_token',
     ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,33 +4,57 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/*
+|--------------------------------------------------------------------------
+| 最终该迁移文件生成的用户表字段信息如下
+|--------------------------------------------------------------------------
+|
+|   字段名称                    字段类型                特性
+|   id                          integer
+|   name                        string
+|   email                       string                 key
+|   email_verified_at           datetime               可为空
+|   password                    string
+|   remember_token              string
+|   created_at                  datetime
+|   updated_at                  datetime
+|
+|--------------------------------------------------------------------------
+*/
+
+
+
+
+
+
+
 class CreateUsersTable extends Migration
 {
     /**
-     * Run the migrations.
+     * 运行迁移时，up被调用
      *
      * @return void
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+        Schema::create('users', function (Blueprint $table) { //创建users表,$table为Blueprint实例的闭包
+            $table->id();   //id()是bigIncrements()的封装，此方法创建了一个bigintunsigned类型的自增长id
+            $table->string('name');     //创建VARCHAR类型的name字段
+            $table->string('email')->unique();    //unique()指定该字段的值为唯一值
+            $table->timestamp('email_verified_at')->nullable();     //nullable表示该字段可为空
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken();    //创建一个remember_token字段用于保存“记住我”的相关信息
+            $table->timestamps();       //创建了一个created_at和updated_at字段，分别用于保存用户的创建时间和更新时间
         });
     }
 
     /**
-     * Reverse the migrations.
+     * 回滚迁移时，down被调用
      *
      * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users');  //删除users表
     }
 }
