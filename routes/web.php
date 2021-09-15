613397e8bec4ef34fbd1f9871f8d32cbfe7e5523
Hershel Zhang <wszhhx@163.com>
2021-09-15 13:48:48 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('/signup', 'UsersController@create')->name('signup');
 
-Route::get('/signup', 'UserController@create')->name('signup');
+Route::resource('/users', 'UsersController');
