1f4cc8156770126e53ab902c8f7bb96fd77f17a8
Hershel Zhang <wszhhx@163.com>
2021-09-15 14:08:26 +0000

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.default')
 @section('title', '注册')
-
+<!--Laravel 提供了全局辅助函数 old 来帮助我们在 Blade 模板中显示旧输入数据。这样当我们信息填写错误，页面进行重定向访问时，输入框将自动填写上最后一次输入过的数据。-->
 @section('content')
-<h1>注册</h1>
+<div class="offset-md-2 col-md-8">
+  <div class="card">
+    <div class="card-header">
+      <h5>注册</h5>
+    </div>
+    <div class="card-body">
+      <form method="POST" action="{{ route('users.store') }}">
+        <div class="form-group">
+          <label for="name">名称</label>
+          <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="email">邮箱</label>
+          <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="password">密码</label>
+          <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="password_confirmation">确认密码</label>
+          <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">注册</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
